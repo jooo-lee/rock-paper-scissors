b3908caf6aa1f2f75b126cddfe0b30f852dc8fb9
Joseph Lee <josephlee3004@gmail.com>
2023-01-01 14:06:22 -0500

    Write playRound function pseudocode

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ function getComputerChoice() {
     return choices[randInt];
 }
 
-console.log(getComputerChoice());
+// play a single round of RPS
+function playRound(playerSelection, computerSelection) {
+    // compare playerSelection with computerSelection
+    // if selections are equal => tie
+    // else compare:
+    //      (player = rock, comp = scissors) || (player = scissors, comp = paper) || 
+    //         (player = paper, comp = rock) => player wins
+    //      (player = rock, comp = paper) || (player = scissors, comp = rock) ||
+    //         (player = paper, comp = scissors) => player loses
+    // return result of round as a string
+}
+
+// For testing later on
+// const playerSelection = "Rock";
+// const computerSelection = getComputerChoice();
+
+// console.log(playRound(playerSelection, computerSelection));
